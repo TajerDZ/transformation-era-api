824f565e2cae3f44e19df2e21296c504813ea5b7
hicham lehouedj <hicham5lehouedj@gmail.com>
2025-05-10 13:37:12 +0100

    fix some errors

--- src/index.ts
+++ src/index.ts
@@ -134,7 +134,7 @@ export const pubsub = new PubSub();
     try {
         const MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb+srv://hicham5lehouedj:Hicham0675722241h@cluster2.ajmndot.mongodb.net/transformation-era?retryWrites=true&w=majority&appName=Cluster2"
         console.log({MONGO_DB_URL})
-        await mongoose.connect("mongodb://root:teLOkMeuvvEyYwsl4X8d7xMbb3ewfPOsg3lEJKunUHyz2ISpWYhGbJzBJ1SjkADt@147.93.108.152:5550/?directConnection=true");
+        await mongoose.connect("mongodb://root:teLOkMeuvvEyYwsl4X8d7xMbb3ewfPOsg3lEJKunUHyz2ISpWYhGbJzBJ1SjkADt@147.93.108.152:5550/transformation-era?directConnection=true");
         console.log('MongoDB Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
